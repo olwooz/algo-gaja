577ed0157f1c3d065e614b5d036d06baefed5a32
kwaksj329 <kwaksj329@gmail.com>
2023-08-31 14:32:17 +0900

    Refactor 과일 장수

--- Programmers/230830_과일_장수/kwaksj329.js
+++ Programmers/230830_과일_장수/kwaksj329.js
@@ -1,12 +1,6 @@
 function solution(k, m, score) {
   let answer = 0;
   score.sort((a, b) => b - a);
-  let remain = score.length % m;
-  if (remain !== 0) {
-    for (let i = 0; i < remain; i++) {
-      score.pop();
-    }
-  }
   for (let j = m - 1; j < score.length; j += m) {
     answer += score[j] * m;
   }
