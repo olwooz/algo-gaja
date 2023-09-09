037f11e2edd59eb7f9f1365e0645afba01c8c270
kwaksj329 <kwaksj329@gmail.com>
2023-09-09 19:24:00 +0900

    Create 게임 맵 최단거리

---
+++ Programmers/230906_게임_맵_최단거리/kwaksj329.js
@@ -0,0 +1,37 @@
+function solution(maps) {
+  let m = Array(maps.length)
+    .fill()
+    .map(() => Array(maps[0].length).fill(false));
+  m[0][0] = true;
+
+  let queue = [];
+  const dx = [-1, 0, 0, 1];
+  const dy = [0, -1, 1, 0];
+  queue.push([0, 0, 1]);
+
+  while (queue.length > 0) {
+    let [x, y, depth] = queue.shift();
+    for (let i = 0; i < 4; i++) {
+      let nx = x + dx[i];
+      let ny = y + dy[i];
+      if (
+        nx >= 0 &&
+        ny >= 0 &&
+        nx < m[0].length &&
+        ny < m.length &&
+        maps[ny][nx] == 1 &&
+        !m[ny][nx]
+      ) {
+        if (nx == m[0].length - 1 && ny == m.length - 1) {
+          return ++depth;
+        }
+        queue.push([nx, ny, depth + 1]);
+        m[ny][nx] = true;
+      }
+    }
+  }
+
+  return -1;
+}
+
+console.log(solution([[1, 1, 1, 1, 1]]));
