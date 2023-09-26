113a79b783a92b6234f3d4639c0ab2677cca94f5
dyi919 <ehddjs13579@gmail.com>
2023-09-26 17:55:38 +0900

    Create 숫자 변환하기

---
+++ Programmers/230924_숫자_변환하기/olwooz.js
@@ -0,0 +1,21 @@
+function solution(x, y, n) {
+  const queue = [y];
+  const cache = Array(y + 1).fill(-1);
+
+  cache[y] = 0;
+
+  while (queue.length > 0) {
+    const current = queue.shift();
+
+    for (const next of [current - n, current / 2, current / 3]) {
+      if (next === x) return cache[current] + 1;
+
+      if (Number.isInteger(next) && cache[next] === -1) {
+        cache[next] = cache[current] + 1;
+        queue.push(next);
+      }
+    }
+  }
+
+  return cache[x];
+}
