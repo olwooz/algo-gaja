6b7cddc106875a689b4d666959a33cca24d13d2d
dyi919 <ehddjs13579@gmail.com>
2024-03-05 23:13:46 +0900

    Create 피로도

---
+++ Programmers/240305_피로도/olwooz.js
@@ -0,0 +1,18 @@
+function solution(k, dungeons, count = 0) {
+  let answer = count;
+
+  for (let i = 0; i < dungeons.length; i++) {
+    if (k < dungeons[i][0]) return answer;
+
+    answer = Math.max(
+      answer,
+      solution(
+        k - dungeons[i][1],
+        [...dungeons.slice(0, i), ...dungeons.slice(i + 1)],
+        count + 1
+      )
+    );
+  }
+
+  return answer;
+}
